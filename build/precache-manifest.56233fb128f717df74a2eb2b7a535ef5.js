53e8e166ddb44fd865cdf5ccb962e6e416036c19
Ayoub Boulahtar <ayoub.boulahtar@gmail.com>
2021-05-11 18:32:55 +0000

    first commit

---
+++ build/precache-manifest.56233fb128f717df74a2eb2b7a535ef5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6396221040c631bc81a0b58c63faa706",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a838ea8b3e6a831edec",
+    "url": "/static/css/main.e23e6b48.chunk.css"
+  },
+  {
+    "revision": "7ccb9089acef15d5ec5f",
+    "url": "/static/js/2.58a8787f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.58a8787f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a838ea8b3e6a831edec",
+    "url": "/static/js/main.b30f5bbc.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  }
+]);
